f0fcfa08ba2c1c6f483449b0d7ba6d0f3468c8ac
rangga <ranggahalimperdana@gmail.com>
2024-05-13 15:50:00 +0700

    tes kongigurasi rangga

--- src/App.vue
+++ src/App.vue
@@ -4,27 +4,19 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <h2 style="font-family: 'Space Mono', monospace;">Hello, World!</h2>
+    <h2 style="font-family: 'Space Mono', monospace;">ini saya!</h2>
+    
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+<script>
+export default {
+  name: 'App',
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+</script>
+
+<style>
+/* CSS untuk font Space Mono */
+@import url('https://fonts.googleapis.com/css2?family=Space+Mono&display=swap');
 </style>
